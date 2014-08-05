9da335706845d004f6d3b9fea6b7c9193cc5a986
Ian McGregor <ian@stinkdigital.com>
2014-08-05 18:19:48 +0100

    using new audio lib

--- src/js/main.js
+++ src/js/main.js
@@ -1,30 +1,28 @@
 'use strict';
 
-var THREE = require('THREE'),
+var Sono = require('./lib/sono.js'),
+    THREE = require('THREE'),
     ThreeBase = require('./utils/three-base.js'),
-    AssetLoader = require('Lib').AssetLoader,
-    WebAudio = require('./utils/web-audio.js'),
     Room = require('./view/room.js'),
     Hero = require('./view/hero.js'),
     ready = require('Lib').ready;
 
-function AudioRoom() {
+function ThreeAudio() {
     ThreeBase.call(this, 45);
 
     this.createScene();
     this.createLights();
+    this.createAudio();
 
     this.camera.position.set(0, 80, 400);
 
     this.render();
-
-    this.addAudio();
 }
 
-AudioRoom.prototype = Object.create(ThreeBase.prototype);
-AudioRoom.prototype.constructor = AudioRoom;
+ThreeAudio.prototype = Object.create(ThreeBase.prototype);
+ThreeAudio.prototype.constructor = ThreeAudio;
 
-AudioRoom.prototype.update = function(deltaTime, elapsedTime) {
+ThreeAudio.prototype.update = function(deltaTime, elapsedTime) {
     this.hero.preUpdate(deltaTime, elapsedTime);
 
     var collision = this.hero.collide(this.room.walls);
@@ -44,14 +42,16 @@ AudioRoom.prototype.update = function(deltaTime, elapsedTime) {
     this.hero.update();
 };
 
-AudioRoom.prototype.createScene = function() {
+ThreeAudio.prototype.createScene = function() {
     this.room = new Room(512, 2048, 256);
     this.scene.add(this.room);
 
     var materials = [];
     var faces = ['left', 'right', 'top', 'bottom', 'front', 'back'];
     for ( var i = 0; i < faces.length; i ++ ) {
-        materials.push( new THREE.MeshPhongMaterial( { map: THREE.ImageUtils.loadTexture( 'img/' + faces[i] + '.png') } ) );
+        var tex = THREE.ImageUtils.loadTexture( 'img/' + faces[i] + '.png');
+        var mat = new THREE.MeshPhongMaterial({ map: tex });
+        materials.push(mat);
     }
 
     this.hero = new Hero(materials);
@@ -67,7 +67,7 @@ AudioRoom.prototype.createScene = function() {
     document.body.appendChild(info);
 };
 
-AudioRoom.prototype.createLights = function() {
+ThreeAudio.prototype.createLights = function() {
     this.light = new THREE.PointLight(0xffffff, 1, 2000);
     this.light.position.set(50, 64, 1024);
     this.scene.add(this.light);
@@ -82,44 +82,33 @@ AudioRoom.prototype.createLights = function() {
     this.scene.add(this.directionalLight);
 };
 
-AudioRoom.prototype.updateAudio = function() {
-    if(this.panner) {
-        this.audio.helpers.setListenerOrientation(this.hero.forward);
-        this.audio.helpers.setListenerPosition(this.hero.position);
-        //console.log(this.hero.position.distanceTo(this.speaker.position));
-    }
-};
-
-AudioRoom.prototype.addAudio = function() {
-    this.audio = new WebAudio();
-    this.loader = new AssetLoader();
-    this.loader.webAudioContext = this.audio.context;
-    this.loader.add('./audio/DRUMS' + WebAudio.getExtension());
-    this.loader.onChildComplete.add(this.audioLoaded, this);
-    this.loader.start();
-};
-
-AudioRoom.prototype.audioLoaded = function(file) {
-    this.audio.add(file.data);
-
-    this.panner = this.audio.addNode(this.audio.nodeFactory.pan());
-    this.panner.distanceModel = 'linear'; // 'linear' 'inverse' 'exponential'
-    this.panner.refDistance = 1;
-    this.panner.maxDistance = 1000;
-    this.panner.rolloffFactor = 1;
-
-    /*this.panner.coneOuterGain = 0.5;
-    this.panner.coneOuterAngle = 180;
-    this.panner.coneInnerAngle = 0;*/
-
-    // set the audio position to the speaker
+ThreeAudio.prototype.createAudio = function() {
+    this.sound = Sono.load('drums', ['audio/DRUMS.ogg', 'audio/DRUMS.mp3'], true);
+    // add a panner node
+    var panner = this.sound.addNode(Sono.create.pan());
+    panner.distanceModel = 'linear'; // 'linear' 'inverse' 'exponential'
+    panner.refDistance = 1;
+    panner.maxDistance = 1000;
+    panner.rolloffFactor = 1;
+    /*panner.coneOuterGain = 0.5;
+    panner.coneOuterAngle = 180;
+    panner.coneInnerAngle = 0;*/
+    // get pan helper util
+    this.pan = Sono.utils.panHandler(panner);
+    // set the audio position and orientation to forward vec of speaker
     var p = this.speaker.position.clone().normalize();
-    this.audio.helpers.setSourcePosition(this.panner, this.speaker.position);
-    this.audio.helpers.setSourceOrientation(this.panner, p);
+    this.pan.setSourcePosition(this.speaker.position);
+    this.pan.setSourceOrientation(p);
+    // play
+    this.sound.play();
+};
 
-    this.audio.play();
+ThreeAudio.prototype.updateAudio = function() {
+    // set listener position and orientation to hero vec
+    this.pan.setListenerOrientation(this.hero.forward);
+    this.pan.setListenerPosition(this.hero.position);
 };
 
 ready(function() {
-    new AudioRoom();
+    new ThreeAudio();
 });
